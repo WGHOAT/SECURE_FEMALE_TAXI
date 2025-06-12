3a7a2a8d6cf554d4a5fef0d2fba79382e1f5c7d2
WGHOAT <ajayraj180801@gmail.com>
2025-06-12 20:07:01 +0530

    Intial_message to the other developer

---
+++ Driver_recognition_module.py
@@ -0,0 +1,12 @@
+"""
+I know , I know Your ass Would open this after checking the name
+
+Bitch , We havent Done any shit other than finding a name for this fuck
+Please Do anything
+
+I will somehow prepare the Database we will do next things after  
+
+
+Yours Truely 
+
+Another Fuck"""
