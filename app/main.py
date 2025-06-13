0674ca9988e0d13da5da33f9eb80284f6ec39664
WGHOAT <ajayraj180801@gmail.com>
2025-06-13 13:46:50 +0530

    instructions

--- app/main.py
+++ app/main.py
@@ -6,6 +6,23 @@ app = FastAPI()
 now when you go to http://127.0.0.1:8000/(the app.get() values you can access the api) 
 
 example: http://127.0.0.1:8000/test_case/1
+
+
+also when you push this to main branch do like this:
+
+ 1243  git add .
+ 1244  git commit -m 'FASTAPI_INTIALIZATION'
+ 1245  git pull origin main
+ 1246  git push -u origin main
+
+ first add -> commit -> pull -> push
+
+
+ that commit will save your work locally 
+
+ and pull will keep you upto date 
+
+ and push will push it to the main branch
 """
 
 
